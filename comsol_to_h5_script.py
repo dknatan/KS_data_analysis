054c3c1b5c79e4d9aa18947e555fb6f801fde35c
dknatan <natandominko@gmail.com>
2024-04-24 16:11:57 +0200

    wednesday update

--- comsol_to_h5_script.py
+++ comsol_to_h5_script.py
@@ -1,7 +1,8 @@
 import comsol_to_h5
 
 # get pandas df from ';'-separated csv file
-data_df = comsol_to_h5.read_from_comsol_native_csv("/scratch/n/Natan.Dominko/comsol_data/test.txt")
+data_df = comsol_to_h5.read_from_comsol_native_csv("/scratch/n/Natan.Dominko/comsol_data/logistic_t_1e5_eps_1e2_1.txt")
+# data_df = comsol_to_h5.read_from_comsol_native_csv("/home/n/Natan.Dominko/Downloads/test42.txt")
 
 # convert csv file to dict that structures into ahierarchy of 
 # {
@@ -14,6 +15,7 @@ data_df = comsol_to_h5.read_from_comsol_native_csv("/scratch/n/Natan.Dominko/com
 # }
 structured_data = comsol_to_h5.extract_data_frames_grouped_by_variable_name_parameters_and_time_steps(data_df)
 # write the structured data to file
-comsol_to_h5.write_data_to_h5(data_df, "/scratch/n/Natan.Dominko/h5_data/test.h5")
+comsol_to_h5.write_data_to_h5(data_df, "/scratch/n/Natan.Dominko/h5_data/logistic_t_1e5_eps_1e2_1.h5")
+# comsol_to_h5.write_data_to_h5(data_df, "/home/n/Natan.Dominko/Downloads/test42.h5")
 
 
